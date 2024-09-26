f86b06cab1dfa8afd2024bb600e42cde947b68c1
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-26 23:36:03 +0200

    Create tests for methods #gridFull? and #play, add tests for method #addToken

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -31,10 +31,20 @@ describe ConnectFour do
         expect(game.addToken(0, :x)).to eq(false)
       end
     end
+
+    context "when the column is invalid" do
+      before do
+        allow(game).to receive(:puts)
+      end
+
+      it "returns false" do
+        expect(game.addToken(-2, :x)).to eq(false)
+      end
+    end
   end
 
   describe "#checkVerticalFour" do
-    context "when there are four vertical identical connected tokens" do
+    context "when there is a vertical four" do
       it "returns true" do
         grid = game.instance_variable_get(:@grid)
         grid[6] = %i[b a a a a]
@@ -42,7 +52,7 @@ describe ConnectFour do
       end
     end
 
-    context "when there are no four vertical identical connected tokens" do
+    context "when there is no vertical four" do
       it "returns false" do
         grid = game.instance_variable_get(:@grid)
         grid[6] = %i[a a a b a]
@@ -52,7 +62,7 @@ describe ConnectFour do
   end
 
   describe "#checkHorizontalFour" do
-    context "when there are four horizontal identical connected tokens" do
+    context "when there is a horizontal four" do
       it "returns true" do
         grid = game.instance_variable_get(:@grid)
         (1..4).each { |i| grid[i] << :x }
@@ -60,7 +70,7 @@ describe ConnectFour do
       end
     end
 
-    context "when there are no four horizontal identical connected tokens" do
+    context "when there is no horizontal four" do
       it "returns false" do
         grid = game.instance_variable_get(:@grid)
         (0..2).each { |i| grid[i] << :x }
@@ -71,7 +81,7 @@ describe ConnectFour do
   end
 
   describe "#checkDiagonalFour" do
-    context "when there are four diagonal identical connected tokens" do
+    context "when there is a diagonal four" do
       it "returns true" do
         grid = game.instance_variable_get(:@grid)
         grid[1] = %i[x]
@@ -82,7 +92,7 @@ describe ConnectFour do
       end
     end
 
-    context "when there are no four diagonal identical connected tokens" do
+    context "when there is no diagonal four" do
       it "returns false" do
         grid = game.instance_variable_get(:@grid)
         grid[1] = %i[x]
@@ -90,6 +100,62 @@ describe ConnectFour do
         grid[3] = %i[x y x]
         grid[4] = %i[x x y y x]
         expect(game.checkDiagonalFour).to eq(false)
+      end
+    end
+  end
+
+  describe "#gridFull?" do
+    context "when the grid is full" do
+      it "returns true" do
+        grid = Array.new(7) { Array.new(6, :x) }
+        game.instance_variable_set(:@grid, grid)
+        expect(game.gridFull?).to be(true)
+      end
+    end
+
+    context "when the grid is not full" do
+      it "returns false" do
+        expect(game.gridFull?).to be(false)
+      end
+    end
+  end
+
+  describe "#play" do
+    let(:player1) { {id: "1", symbol: "x"} }
+    let(:player2) { {id: "2", symbol: "o"} }
+
+    context "when the first player wins" do
+      before do
+        allow(game).to receive(:turn)
+        allow(game).to receive(:checkWin).and_return(true)
+      end
+
+      it "returns the first player" do
+        expect(game.play([player1, player2])).to eq(player1)
+      end
+    end
+
+    context "when the second player wins" do
+      before do
+        allow(game).to receive(:turn)
+        allow(game).to receive(:checkWin).and_return(false, true)
+        allow(game).to receive(:gridFull?).and_return(false)
+      end
+
+      it "returns the second player" do
+        expect(game.play([player1, player2])).to eq(player2)
+      end
+    end
+
+    context "when the grid is full" do
+      before do
+        allow(game).to receive(:turn)
+        allow(game).to receive(:checkWin).and_return(false)
+        allow(game).to receive(:gridFull?).and_return(true)
+      end
+
+      it "returns nil" do
+        expect(game.play([player1, player2])).to eq(nil)
       end
     end
   end
