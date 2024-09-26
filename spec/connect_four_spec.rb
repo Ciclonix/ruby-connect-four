3085465b27ddf410ef1a29a06a1179ad570e10f7
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-26 21:42:06 +0200

    Create tests for #addToken

---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative "../lib/connect_four"
+
+describe ConnectFour do
+  subject(:game) { described_class.new }
+
+  describe "#addToken" do
+    context "when the column is not full" do
+      it "returns true" do
+        grid = game.instance_variable_get(:@grid)
+        grid[0] << :y
+        expect(game.addToken(0, :x)).to eq(true)
+      end
+
+      it "adds the token to the top of the column" do
+        grid = game.instance_variable_get(:@grid)
+        grid[0] << :y
+        expect { game.addToken(0, :x) }.to change { grid[0] }.from([:y]).to(%i[y x])
+      end
+    end
+
+    context "when the column is full" do
+      before do
+        allow(game).to receive(:puts)
+      end
+
+      it "returns false" do
+        grid = game.instance_variable_get(:@grid)
+        grid[0] = %i[a b c d e f]
+        expect(game.addToken(0, :x)).to eq(false)
+      end
+    end
+  end
+end
